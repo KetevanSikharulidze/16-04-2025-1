62ec934093577be4682a24fc91ed8c28e0e1be5c
student <student@BTU.local>
2025-04-23 13:32:31 +0400

    tabLayout added

--- app/src/main/java/com/example/myapplication/menuFragments/SongsFragment.kt
+++ app/src/main/java/com/example/myapplication/menuFragments/SongsFragment.kt
@@ -5,16 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.myapplication.R
+import com.example.myapplication.adapters.ViewPagerAdapter
+import com.example.myapplication.databinding.FragmentSongsBinding
+import com.example.myapplication.songsFragments.BlankFragment
+import com.example.myapplication.songsFragments.BlankFragment2
+import com.google.android.material.tabs.TabLayoutMediator
 
 class SongsFragment : Fragment() {
+
+    private var _binding : FragmentSongsBinding? = null
+    private val binding get() = _binding!!
+
+    private val fList = listOf(
+        BlankFragment(),
+        BlankFragment2()
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_songs, container, false)
+    ): View {
+        _binding = FragmentSongsBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding){
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPagerAdapter = ViewPagerAdapter(requireActivity(), fList)
+        vp.adapter = viewPagerAdapter
+
+        val tList = listOf(
+            ContextCompat.getDrawable(requireContext(),R.drawable.ic_equalizer),
+            ContextCompat.getDrawable(requireContext(),R.drawable.ic_forward_arrow)
+        )
+
+        TabLayoutMediator(tabLayout,vp) { tab, position ->
+            tab.icon = tList[position]
+        }.attach()
+
     }
 
     companion object {
